5b81734703f9d355dabcb6e8640cb562e0c9660a
藤井 義己 <gsydinx@gmail.com>
2026-05-17 15:06:34 +0900

    feat: implement multi-device session connectivity via integrated HTTP server and connection pages

--- fluid-general.Avalonia/Pages/ConnectPage.axaml.cs
+++ fluid-general.Avalonia/Pages/ConnectPage.axaml.cs
@@ -135,7 +135,7 @@ public partial class ConnectPage : UserControl
     {
         if (ParentListBox.SelectedItem is ParentInfo parent)
         {
-            string url = $"http://{parent.Ip}:5000/";
+            string url = $"http://{parent.Ip}:5010/";
             await ProcessConnectionAsync(url);
         }
     }
